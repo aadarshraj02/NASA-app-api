4e60386a1b20bbf2e4dc0ed5593ba0eb3f430411
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-04 13:59:34 +0530

    updated button text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import { BsInfoCircleFill } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full flex justify-between items-center gap-4 text-zinc-400">
+    <footer className="fixed bottom-0 left-0 w-full flex justify-between items-center gap-4 text-zinc-400 p-4">
       <div>
         <h2 className="text-xl ">The Brutal Martin Landscape</h2>
         <h1 className="text-3xl">APOD Project</h1>
       </div>
       <div>
-        <button>
+        <button className="text-xl">
           <BsInfoCircleFill />
         </button>
       </div>
