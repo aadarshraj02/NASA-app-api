a748a76b0abba1c494391b02dcccca4f2a40df0e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-04 15:41:21 +0530

    displayed title

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,11 @@
 import { IoClose } from "react-icons/io5";
 
-const Sidebar = ({ handleClose }) => {
+const Sidebar = ({ handleClose, data }) => {
   return (
     <div className="fixed inset-0 flex flex-col text-zinc-400">
       <div className="flex flex-col gap-4 p-4 h-full w-[40%] max-w-[800px] ml-auto bg-zinc-800 overflow-y-scroll">
         <h2 className="text-3xl text-center text-zinc-200 font-bold">
-          The Brutal Martin Landscape
+          {data?.title}
         </h2>
         <div className="flex-1">
           <p className="text-xl text-zinc-300">Description</p>
