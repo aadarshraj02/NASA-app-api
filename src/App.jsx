12f146f83a6458bddbc3c7b61aa684fe1c4720d6
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-04 10:49:23 +0530

    imported sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <div>
+      <Sidebar></Sidebar>
       <Main />
       <Footer />
     </div>
