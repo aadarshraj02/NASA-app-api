2e0f21039aca01033eef06bfd0c8ae98c80ffac9
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-04 12:54:47 +0530

    styled sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,16 @@
 const Sidebar = () => {
   return (
-    <div>
-      <h2>The Brutal Martin Landscape</h2>
-      <div>
-        <p>Description</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aut
-          quia vitae mollitia culpa facilis, sapiente atque. Debitis, soluta et.
-        </p>
+    <div className="fixed inset-0 flex flex-col">
+      <div className="flex flex-col gap-4 p-4 h-full w-[90%] max-w-[800px] ml-auto bg-zinc-800 overflow-y-scroll">
+        <h2>The Brutal Martin Landscape</h2>
+        <div>
+          <p>Description</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aut
+            quia vitae mollitia culpa facilis, sapiente atque. Debitis, soluta
+            et.
+          </p>
+        </div>
       </div>
     </div>
   );
