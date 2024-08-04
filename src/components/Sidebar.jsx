75aa15be0eb99b554674b7403ccb9a79b49a6444
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-04 12:17:08 +0530

    added h2 for header and description of pic in p

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,16 @@
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>The Brutal Martin Landscape</h2>
+      <div>
+        <p>Description</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aut
+          quia vitae mollitia culpa facilis, sapiente atque. Debitis, soluta et.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
