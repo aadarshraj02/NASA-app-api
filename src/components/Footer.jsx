d7f56fd382158ab9c8e3b62629dac32aa9900f02
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-04 12:37:25 +0530

    formatted the code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { BsInfoCircleFill } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="fixed bottom-0 left-0 w-full">
       <div>
         <h2>The Brutal Martin Landscape</h2>
         <h1>APOD Project</h1>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,7 @@
 const Main = () => {
   return (
-    <div className="flex flex-col">
-      <img
-        src="mars.png"
-        alt=""
-        className="w-full h-full flex-1 object-cover"
-      />
+    <div className="flex-1 flex flex-col">
+      <img src="mars.png" alt="" className="w-full h-full object-cover" />
     </div>
   );
 };
