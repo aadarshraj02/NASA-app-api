7b05396f7bbffda6b21665744f3db24b9b843efb
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-04 13:45:37 +0530

    styled h2 and p

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import { IoClose } from "react-icons/io5";
 const Sidebar = () => {
   return (
-    <div className="fixed inset-0 flex flex-col">
-      <div className="flex flex-col gap-4 p-4 h-full w-[90%] max-w-[800px] ml-auto bg-zinc-800 overflow-y-scroll">
-        <h2>The Brutal Martin Landscape</h2>
+    <div className="fixed inset-0 flex flex-col text-zinc-400">
+      <div className="flex flex-col gap-4 p-4 h-full w-[40%] max-w-[800px] ml-auto bg-zinc-800 overflow-y-scroll">
+        <h2 className="text-3xl text-center text-zinc-200 font-bold">
+          The Brutal Martin Landscape
+        </h2>
         <div className="flex-1">
-          <p>Description</p>
-          <p>
+          <p className="text-xl text-zinc-300">Description</p>
+          <p className="text-sm leading-none">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima aut
             quia vitae mollitia culpa facilis, sapiente atque. Debitis, soluta
             et.
