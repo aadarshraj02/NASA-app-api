3e28f2511fa2357d9defc069e2be6a45ab10e0ba
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-04 15:39:04 +0530

    displayed title in the h2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import { BsInfoCircleFill } from "react-icons/bs";
 
 const Footer = (props) => {
-  const { handleDisplayModal } = props;
+  const { handleDisplayModal, data } = props;
 
   return (
     <footer className="fixed bottom-0 left-0 w-full flex justify-between items-center gap-4 text-zinc-400 p-4">
       <div>
-        <h2 className="text-xl ">The Brutal Martin Landscape</h2>
-        <h1 className="text-3xl">APOD Project</h1>
+        <h2 className="text-4xl ">{data?.title}</h2>
+        <h1 className="text-2xl">APOD Project</h1>
       </div>
       <div>
         <button className="text-xl" onClick={handleDisplayModal}>
