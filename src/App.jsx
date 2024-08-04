feda05aa79a96cb0487b990dc3e6b3330fca423d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-04 13:56:18 +0530

    added state to show or hide modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="flex min-h-screen">
       <Main />
-      <Sidebar></Sidebar>
+      {showModal && <Sidebar></Sidebar>}
       <Footer />
     </div>
   );
