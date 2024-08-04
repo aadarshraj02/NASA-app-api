a16a49b2b95b41ff6f1b609c802e1cee2a23c15d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-04 11:20:14 +0530

    added h2 for desc and h1 for title

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>The Brutal Martin Landscape</h2>
+      <h1>APOD Project</h1>
+    </div>
+  );
 };
 
 export default Footer;
